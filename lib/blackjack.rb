a8f102fb7d11249acfe396fb3422b00608d106d1
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-11 19:23:44 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -61,8 +61,13 @@ def runner
   card_total = initial_round
   until card_total >= 21 do
     display_card_total(card_total)
-    hit?(card_total)
+    card_total = hit?(card_total)
   end
-  end_game(card_total)
+  if card_total == 21
+    puts "Congratulations you hit 21!"
+  else
+    end_game(card_total)
+  end
 end
+    
     
